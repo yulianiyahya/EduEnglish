7b4b96d91c72e083b3c8b241febf8748f3e3841a
Reinaldi99 <if23.reinaldisukri@mhs.ubpkarawang.ac.id.com>
2026-04-23 15:04:15 +0700

    tambah splash screen, forgot password, dan fix routing

---
+++ src/app/pages/forgot-password/forgot-password.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordPage } from './forgot-password.page';
+
+const routes: Routes = [
+  { path: '', component: ForgotPasswordPage }
+];
+
+@NgModule({
+  declarations: [ForgotPasswordPage],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ForgotPasswordPageModule {}
